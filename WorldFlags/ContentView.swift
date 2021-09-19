fab26c0b4c4185bad68cfb530b67a76718e72491
Mohamed Alwakil <mdalwakil@outlook.com>
2021-09-19 16:06:37 +0200

    Initial Commit

---
+++ WorldFlags/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WorldFlags
+//
+//  Created by Mohamed Diaa on 2021-09-19.
+//  Copyright © 2021 Mohamed Diaa. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
